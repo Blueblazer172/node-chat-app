d91bfa2a502a1fd8877c1502154627f9bf64c269
Blueblazer172 <hannes.stefani@outlook.de>
2017-02-07 12:04:12 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Hannes',
-        text: 'Test'
-    });
 });
 
 socket.on('disconnect', function() {
